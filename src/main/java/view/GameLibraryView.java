71742d606e009b3f5bb8c81192324485dc8b8a3c
Jiakun <laojiakun@163.com>
2024-11-17 15:35:09 -0500

    Implement search function

--- src/main/java/view/GameLibraryView.java
+++ src/main/java/view/GameLibraryView.java
@@ -8,6 +8,7 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import interface_adapter.change_password.ChangePasswordState;
 import interface_adapter.gamelibrary.GameLibraryController;
@@ -28,9 +29,14 @@ public class GameLibraryView extends JPanel implements PropertyChangeListener {
 
     private final JLabel playerID;
 
-    private final JButton logOut;
+    private final JButton blackJack = new JButton("Black Jack");
+    private final JButton placeHolder = new JButton("Place Holder");
 
-    private final JButton changePassword;
+    private final JButton search = new JButton("Search");
+    private final JTextField searchInputField = new JTextField(15);
+
+    private final JButton logOut = new JButton("Log Out");
+    private final JButton changePassword = new JButton("Change Password");
 
     // @SuppressWarnings("checkstyle:UnusedLocalVariable")
     public GameLibraryView(GameLibraryViewModel gameLibraryViewModel) {
@@ -40,21 +46,22 @@ public class GameLibraryView extends JPanel implements PropertyChangeListener {
         final JLabel title = new JLabel("Game Library Screen");
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        final JPanel game1 = new JPanel();
-        game1.add(new JButton("Black Jack"));
-
         final JLabel playerIDInfo = new JLabel("Currently logged in: ");
         playerID = new JLabel();
 
+        final JPanel searchPanel = new JPanel();
+        searchPanel.add(searchInputField);
+        searchPanel.add(search);
+
+        final JPanel game1 = new JPanel();
+        game1.add(blackJack);
+        game1.add(placeHolder);
+        game1.setLayout(new BoxLayout(game1, BoxLayout.Y_AXIS));
+        game1.setAlignmentX(Component.CENTER_ALIGNMENT);
+
         final JPanel buttons = new JPanel();
-        logOut = new JButton("Log Out");
         buttons.add(logOut);
-
-        changePassword = new JButton("Change Password");
-
-        final JPanel change = new JPanel();
-        change.add(changePassword);
-        buttons.add(change);
+        buttons.add(changePassword);
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
@@ -83,10 +90,24 @@ public class GameLibraryView extends JPanel implements PropertyChangeListener {
                 }
         );
 
+        search.addActionListener(
+                // This creates an anonymous subclass of ActionListener and instantiates it.
+                evt -> {
+                    if (evt.getSource().equals(search)) {
+                        final String info = searchInputField.getText();
+                        for (Component comp: game1.getComponents()) {
+                            final JButton button = (JButton) comp;
+                            setVisible(info, button);
+                        }
+                    }
+                }
+        );
+
         this.add(title);
         this.add(playerIDInfo);
         this.add(playerID);
 
+        this.add(searchPanel);
         this.add(game1);
         this.add(passwordErrorField);
         this.add(buttons);
@@ -111,5 +132,17 @@ public class GameLibraryView extends JPanel implements PropertyChangeListener {
     public void setLogoutController(LogoutController logoutController) {
         // save the logout controller in the instance variable.
         this.logoutController = logoutController;
+    }
+
+    private void setVisible(String info, JButton button) {
+        if ("".equals(info)) {
+            button.setVisible(true);
+        }
+        else if (button.getText().toLowerCase().contains(info.toLowerCase())) {
+            button.setVisible(true);
+        }
+        else {
+            button.setVisible(false);
+        }
     }
 }

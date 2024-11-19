ef73e235029bf82ae55e122ee7dbf01c32df8b21
Estelle(Xiaoyi <xyestelle.wang@mail.utoronto.ca>
2024-11-19 18:22:59 -0500

    Interface Adapters for setDifficulty usecase implemented.

--- src/main/java/interface_adapter/set_difficulty/SetDifficultyController.java
+++ src/main/java/interface_adapter/set_difficulty/SetDifficultyController.java
@@ -1,7 +1,26 @@
 package interface_adapter.set_difficulty;
 
+import use_case.set_difficulty.SetDifficultyInputBoundary;
+import use_case.set_difficulty.SetDifficultyInputData;
+
 /**
  * Controller for the set difficulty use case.
  */
 public class SetDifficultyController {
+    private final SetDifficultyInputBoundary setDifficultyUseCaseInteractor;
+
+    public SetDifficultyController(SetDifficultyInputBoundary setDifficultyUseCaseInteractor) {
+        this.setDifficultyUseCaseInteractor = setDifficultyUseCaseInteractor;
+    }
+
+    /**
+     * Executes the set difficulty Use Case.
+     * @param newDifficulty the new difficulty set by the player.
+     * @param playerID the playerID
+     * @param roomName the roomName
+     */
+    public void execute(String newDifficulty, String playerID, String roomName) {
+        final SetDifficultyInputData setDifficultyInputData = new SetDifficultyInputData(newDifficulty, playerID, roomName);
+        setDifficultyUseCaseInteractor.execute(setDifficultyInputData);
+    }
 }
--- src/main/java/interface_adapter/set_difficulty/SetDifficultyViewModel.java
+++ src/main/java/interface_adapter/set_difficulty/SetDifficultyViewModel.java
@@ -1,4 +1,15 @@
 package interface_adapter.set_difficulty;
 
-public class SetDifficultyViewModel {
+import interface_adapter.ViewModel;
+
+/**
+ * The View Model for the Set Difficulty View.
+ */
+public class SetDifficultyViewModel extends ViewModel<SetDifficultyState> {
+
+    public SetDifficultyViewModel(String viewName) {
+        super("Game Room");
+        setState(new SetDifficultyState());
+    }
+
 }
